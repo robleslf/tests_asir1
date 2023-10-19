335a69d89473577a4c7961e423b16b8acae37398
robleslf <142787149+robleslf@users.noreply.github.com>
2023-10-19 13:35:37 +0200

    Add files via upload

---
+++ test_fundamentos_2.py
@@ -0,0 +1,448 @@
+preguntas = [
+    ("¿Qué es un microprocesador?", [
+        "a) El componente que se encarga de ",
+        "b) Es el componente que se encarga de",
+        "c) Es ",
+        "d) Es el componente principal o cerebro del ordenador."
+    ], "d"),
+    ("¿Con qué nombre se conoce también al microprocesador?", [
+        "a) Procesador",
+        "b) Micro",
+        "c) CPU (o UCP)",
+        "d) Todas las anteriores son correctas"
+    ], "d"),
+    ("¿De qué está compuesto un microprocesador?", [
+        "a) Es un chip con millones de minúsculos elementos electrónicos, casi todos transistores",
+        "b) De la unidad de procesamiento lógico y la unidad de procesamiento físico",
+        "c) De transistores exclusivamente",
+        "d) De millones de elementos minúsculos mecánicos"
+    ], "a"),
+    ("¿Cuál de estas definiciones de Microprocesador es la correcta?", [
+        "a) El microprocesador es el elemento que se encarga de llevar a cabo operaciones matemáticas y lógicas en un corto período de tiempo y también decodifica y ejecuta las instrucciones de los programas cargados en la memoria RAM.",
+        "b) Es el componente principal o cerebro del ordenador",
+        "c) Es un chip que contiene millones de minúsculos elementos electrónicos,casi todos transistores, en un único circuito integrado capaz de realizar las diversas operaciones que se le encomienda al software.",
+        "d) Todas son correctas"
+    ], "d"),
+    ("¿De qué se encarga el microprocesador?", [
+        "a) De llevar a cabo operaciones matemáticas en un corto periodo de tiempo",
+        "b) De llevar a cabo operaciones lógicas en un corto periodo de tiempo",
+        "c) De decodificar y ejecutar las instrucciones de los programas cargados en la memoria RAM",
+        "d) De todas las anteriores"
+    ], "d"),
+    ("¿Qué forma suele tener un microprocesador?", [
+        "a) De cuadrado",
+        "b) De elipse",
+        "c) De tetraedro",
+        "d) Todas las anteriores son incorrectas"
+    ], "a"),
+    ("¿Cómo se llama el zócalo especial de placa base al que se conecta el microprocesador?", [
+        "a) rocket",
+        "b) pocket",
+        "c) socket",
+        "d) CPU-connector"
+    ], "c"),
+    ("¿Qué materiales se emplean para la fabricación de un microprocesaodor?", [
+        "a) Materiales aislantes, como plástico, madera...",
+        "b) Materiales conductores, como cobre, oro...",
+        "c) Materiales semiconductores, como germanio, arsniuro de galio, silicio...",
+        "d) Es indiferente el material empleado."
+    ], "c"),
+    ("¿Por qué a la industria de chips, como los procesadores, se la conoce como Silicon Valley?", [
+        "a) Por la silicona empleada para aislar los microprocesadores",
+        "b) Por el silicio empleado en su fabricación",
+        "c) Porque el lugar fue llamado así por los Conquistadores españoles",
+        "d) Todas las opciones son incorrectas."
+    ], "b"),
+    ("¿Cómo es el proceso de elaboración de los microprocesadores?", [
+        "a) El silicio se forma en lingotes cilíndricos y se corta en discos llamados obleas en las que se graban los microprocesadores. Sobre la oblea se graba un circuito electrónico mediante un proceso de fotolitografía (luz ultravioleta); se realizan multitud de procesos químico-físico.térmicos avanzados hasta obtener la oblea con los chips acabados. Esta se somete a pruebas y se corta en chips individuales. Posteriormente se encapsula el núcleo, se introduce dentro de un material plástico o cerámico y se conecta a sus conectores externos además de añadirle una superficie disipadora.",
+        "b) Simplemente se suelda una oblea de silicio a los diferentes componentes y se somete a luz ultravioleta.",
+        "c) Mediante hilos de silicio.",
+        "d) Ninguna es correcta"
+    ], "a"),
+    ("¿Qué forma tienen los lingotes de silicio utilizados para la elaboración de microprocesadores", [
+        "a) Cilíndrica",
+        "b) Cónica",
+        "c) Prísmica",
+        "d) Cúbica"
+    ], "a"),
+    ("¿Qué se graba y cómo sobre la oblea de silicio a la hora de fabricar un microprocesador", [
+        "a) Se graba un circuito electrónico mediante un proceso de fotolitografía (luz ultravioleta)",
+        "b) Se graba un circuito mecánico mediante un proceso de fotolitografía (luz ultravioleta)",
+        "c) Se graba un circuito electrónico mediante un proceso de soldadura",
+        "d) Se graba un circuito electromecánico mediante un proceso de fotolitografía (luz ultravioleta)"
+    ], "a"),
+    ("¿Qué se hace con los chips individuales una vez cortada la oblea en la fabricación de microprocesadores?", [
+        "a) Se encapsula el núcleo y se introduce dentro de un material plástico o cerámico",
+        "b) Se encapsula el núcleo y se introduce dentro de un material plástico o cerámico; además se conecta a sus conectores externos y se le añade una superficie disipadora",
+        "c) Se encapsula el núcleo y se introduce dentro de un material metálico o de cristal",
+        "d) Se encapsula el núcleo y se introduce dentro de un material metálico o de cristal; además se conecta a sus conectores externos y se le añade una superficie disipadora"
+    ], "b"),
+    ("Cuando observamos el microprocesador, lo que vemos es un cuadrado con contactos eléctricos; esto es...", [
+        "a)  el encapsulado del microprocesador, que es el que protege el chip en si, extremadamente resistente.",
+        "b) el encapsulado del microprocesador, que es el que protege la placa base junto con el propio procesador, extremadamente delicado.",
+        "c) el encapsulado del microprocesador, que es el que protege el chip en si, extremadamente delicado.",
+        "d) el cuadro de mando, que es el que protege el chip en si, extremadamente resistente."
+    ], "c"),
+    ("Entre los diferentes tipos de encapsulado de microprocesadores tenemos:", [
+        "a) DIP, TEX-MEX, PGA y Formatos de ranura",
+        "b) PGA, VGA, SVC y LDD",
+        "c) DIP, Z33, Formatos de ranura y LGA",
+        "d) DIP, PGA, Formatos de ranura y LGA"
+    ], "b"),
+    ("¿", [
+        "a) ",
+        "b) ",
+        "c) ",
+        "d) "
+    ], "b"),
+    ("¿", [
+        "a) ",
+        "b) ",
+        "c) ",
+        "d) "
+    ], "b"),
+    ("¿", [
+        "a) ",
+        "b) ",
+        "c) ",
+        "d) "
+    ], "b"),
+    ("¿", [
+        "a) ",
+        "b) ",
+        "c) ",
+        "d) "
+    ], "b"),
+        ("¿", [
+        "a) ",
+        "b) ",
+        "c) ",
+        "d) "
+    ], "b"),
+    ("¿", [
+        "a) ",
+        "b) ",
+        "c) ",
+        "d) "
+    ], "b"),
+    ("¿", [
+        "a) ",
+        "b) ",
+        "c) ",
+        "d) "
+    ], "b"),
+    ("¿", [
+        "a) ",
+        "b) ",
+        "c) ",
+        "d) "
+    ], "b"),
+    ("¿", [
+        "a) ",
+        "b) ",
+        "c) ",
+        "d) "
+    ], "b"),
+    ("¿", [
+        "a) ",
+        "b) ",
+        "c) ",
+        "d) "
+    ], "b"),
+    ("¿", [
+        "a) ",
+        "b) ",
+        "c) ",
+        "d) "
+    ], "b"),
+    ("¿", [
+        "a) ",
+        "b) ",
+        "c) ",
+        "d) "
+    ], "b"),
+    ("¿", [
+        "a) ",
+        "b) ",
+        "c) ",
+        "d) "
+    ], "b"),
+    ("¿", [
+        "a) ",
+        "b) ",
+        "c) ",
+        "d) "
+    ], "b"),
+    ("¿", [
+        "a) ",
+        "b) ",
+        "c) ",
+        "d) "
+    ], "b"),
+    ("¿", [
+        "a) ",
+        "b) ",
+        "c) ",
+        "d) "
+    ], "b"),
+    ("¿", [
+        "a) ",
+        "b) ",
+        "c) ",
+        "d) "
+    ], "b"),
+    ("¿", [
+        "a) ",
+        "b) ",
+        "c) ",
+        "d) "
+    ], "b"),    ("¿", [
+        "a) ",
+        "b) ",
+        "c) ",
+        "d) "
+    ], "b"),
+    ("¿", [
+        "a) ",
+        "b) ",
+        "c) ",
+        "d) "
+    ], "b"),
+    ("¿", [
+        "a) ",
+        "b) ",
+        "c) ",
+        "d) "
+    ], "b"),
+    ("¿", [
+        "a) ",
+        "b) ",
+        "c) ",
+        "d) "
+    ], "b"),
+    ("¿", [
+        "a) ",
+        "b) ",
+        "c) ",
+        "d) "
+    ], "b"),
+    ("¿", [
+        "a) ",
+        "b) ",
+        "c) ",
+        "d) "
+    ], "b"),
+    ("¿", [
+        "a) ",
+        "b) ",
+        "c) ",
+        "d) "
+    ], "b"),
+    ("¿", [
+        "a) ",
+        "b) ",
+        "c) ",
+        "d) "
+    ], "b"),
+    ("¿", [
+        "a) ",
+        "b) ",
+        "c) ",
+        "d) "
+    ], "b"),
+    ("¿", [
+        "a) ",
+        "b) ",
+        "c) ",
+        "d) "
+    ], "b"),
+    ("¿", [
+        "a) ",
+        "b) ",
+        "c) ",
+        "d) "
+    ], "b"),
+    ("¿", [
+        "a) ",
+        "b) ",
+        "c) ",
+        "d) "
+    ], "b"),
+    ("¿", [
+        "a) ",
+        "b) ",
+        "c) ",
+        "d) "
+    ], "b"),
+    ("¿", [
+        "a) ",
+        "b) ",
+        "c) ",
+        "d) "
+    ], "b"),    ("¿", [
+        "a) ",
+        "b) ",
+        "c) ",
+        "d) "
+    ], "b"),
+    ("¿", [
+        "a) ",
+        "b) ",
+        "c) ",
+        "d) "
+    ], "b"),
+    ("¿", [
+        "a) ",
+        "b) ",
+        "c) ",
+        "d) "
+    ], "b"),
+    ("¿", [
+        "a) ",
+        "b) ",
+        "c) ",
+        "d) "
+    ], "b"),
+    ("¿", [
+        "a) ",
+        "b) ",
+        "c) ",
+        "d) "
+    ], "b"),
+    ("¿", [
+        "a) ",
+        "b) ",
+        "c) ",
+        "d) "
+    ], "b"),
+    ("¿", [
+        "a) ",
+        "b) ",
+        "c) ",
+        "d) "
+    ], "b"),
+    ("¿", [
+        "a) ",
+        "b) ",
+        "c) ",
+        "d) "
+    ], "b"),
+    ("¿", [
+        "a) ",
+        "b) ",
+        "c) ",
+        "d) "
+    ], "b"),
+    ("¿", [
+        "a) ",
+        "b) ",
+        "c) ",
+        "d) "
+    ], "b"),
+    ("¿", [
+        "a) ",
+        "b) ",
+        "c) ",
+        "d) "
+    ], "b"),
+    ("¿", [
+        "a) ",
+        "b) ",
+        "c) ",
+        "d) "
+    ], "b"),
+    ("¿", [
+        "a) ",
+        "b) ",
+        "c) ",
+        "d) "
+    ], "b"),
+    ("¿", [
+        "a) ",
+        "b) ",
+        "c) ",
+        "d) "
+    ], "b"),    ("¿", [
+        "a) ",
+        "b) ",
+        "c) ",
+        "d) "
+    ], "b"),
+    ("¿", [
+        "a) ",
+        "b) ",
+        "c) ",
+        "d) "
+    ], "b"),
+    ("¿", [
+        "a) ",
+        "b) ",
+        "c) ",
+        "d) "
+    ], "b"),
+    ("¿", [
+        "a) ",
+        "b) ",
+        "c) ",
+        "d) "
+    ], "b"),
+    ("¿", [
+        "a) ",
+        "b) ",
+        "c) ",
+        "d) "
+    ], "b"),
+    ("¿", [
+        "a) ",
+        "b) ",
+        "c) ",
+        "d) "
+    ], "b"),
+    ("¿", [
+        "a) ",
+        "b) ",
+        "c) ",
+        "d) "
+    ], "b"),
+    ("¿", [
+        "a) ",
+        "b) ",
+        "c) ",
+        "d) "
+    ], "b"),
+    ("¿", [
+        "a) ",
+        "b) ",
+        "c) ",
+        "d) "
+    ], "b"),
+    ("¿", [
+        "a) ",
+        "b) ",
+        "c) ",
+        "d) "
+    ], "b"),
+    ("¿", [
+        "a) ",
+        "b) ",
+        "c) ",
+        "d) "
+    ], "b"),
+    ("¿", [
+        "a) ",
+        "b) ",
+        "c) ",
+        "d) "
+    ], "b"),
+    ("¿", [
+        "a) ",
+        "b) ",
+        "c) ",
+        "d) "
+    ], "b"),
+    ("¿", [
+        "a) ",
+        "b) ",
+        "c) ",
+        "d) "
+    ], "b"),
